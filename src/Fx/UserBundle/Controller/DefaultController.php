85df3b91beef6de8f9cc915842913fd79abc749f
ffouillet <felix.fouillet@gmail.com>
2018-08-23 12:28:53 +0200

    Added http basic users authentication, Closes #1

--- src/Fx/UserBundle/Controller/DefaultController.php
+++ src/Fx/UserBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Fx\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class DefaultController extends Controller
-{
+{	
+
     public function indexAction()
     {
-        return $this->render('FxUserBundle:Default:index.html.twig');
+        return $this->render('fx/blog/index.html.twig');
     }
+
 }
